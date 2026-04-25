21b3e76cff24674d6ffd4cecc0366aa4e8a6b536
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2026-04-25 21:52:14 +0545

    Remove: Time slot

--- database/migrations/2024_10_04_080724_create_therapists_table.php
+++ database/migrations/2024_10_04_080724_create_therapists_table.php
@@ -1,34 +1,31 @@
-<?php 
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
-{
-    Schema::create('therapists', function (Blueprint $table) {
-        $table->engine = 'InnoDB';
-        $table->id();
-        $table->string('name');
-        $table->text('description');
-        $table->string('location');
-        $table->string('experience');
-        $table->enum('status', ['Available', 'Not-Available']);
-        $table->string('photopath')->nullable();
-        $table->foreignId('specialist_id')->unsigned(); // Foreign key
-        $table->decimal('fee', 8, 2);
-        $table->json('time_slot')->nullable();
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('therapists', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
 
-    /**
-     * Reverse the migrations.
-     */
+            // Profile Details
+            $table->string('name');
+            $table->text('description');
+            $table->string('location');
+            $table->string('experience');
+            $table->enum('status', ['Available', 'Not-Available'])->default('Available');
+            $table->string('photopath')->nullable();
+            $table->foreignId('specialist_id')->unsigned(); 
+            $table->decimal('fee', 8, 2);
+
+            $table->timestamps();
+        });
+    }
+
     public function down(): void
     {
         Schema::dropIfExists('therapists');
